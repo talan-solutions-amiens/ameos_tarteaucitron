83e34fec65d5d63c12917fd279a5197fe4c9f13e
Oliver Singery <oliver.singery@talan.com>
2019-12-05 17:26:23 +0100

    Fix (Pardot integration): Constants, setup + custom services

---
+++ Resources/Public/js/customservices.js
@@ -0,0 +1,28 @@
+tarteaucitron.services.pardot = {
+    "key": "pardot",
+    "type": "ads",
+    "name": "Pardot",
+    "needConsent": true,
+    "cookies": [],
+    "js": function () {
+        "use strict";
+        // When user allow cookie
+        var piAId = tarteaucitron.user.pardotPiAId;
+        var piCId = tarteaucitron.user.pardotPiCId;
+        var piHostname = tarteaucitron.user.pardotPiHostname;
+
+        (function() {
+            function async_load(){
+                var s = document.createElement('script'); s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+            }
+            if(window.attachEvent) { window.attachEvent('onload', async_load); }
+            else { window.addEventListener('load', async_load, false); }
+        })();
+    },
+    "fallback": function () {
+        "use strict";
+        // when use deny cookie
+    }
+};
