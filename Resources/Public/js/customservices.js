8f8bc80746e67860be6b00ee1d2ddc7f780ed1c3
Oliver Singery <oliver.singery@talan.com>
2019-12-10 14:35:04 +0100

    Feat (Pardot): Pardot async already

--- Resources/Public/js/customservices.js
+++ Resources/Public/js/customservices.js
@@ -17,8 +17,12 @@ tarteaucitron.services.pardot = {
                 s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
                 var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
             }
-            if(window.attachEvent) { window.attachEvent('onload', async_load); }
-            else { window.addEventListener('load', async_load, false); }
+            if (document.readyState === "complete") {
+                async_load();
+            } else {
+                if(window.attachEvent) { window.attachEvent('onload', async_load); }
+                else { window.addEventListener('load', async_load, false); }
+            }
         })();
     },
     "fallback": function () {
